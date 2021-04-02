c569b1134a8fbb2ee04a0a46346f2cc866efedfd
Rash-mirash <disharllet@gmail.com>
2021-04-03 00:30:17 +0300

    added meny section

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,22 @@ $(function() {
     dots: true,
     arrows: false
   });
-  var mixer = mixitup('.product__content');
+
+  mixitup('.product__content');
+  mixitup('.desing__content');
+
+  $(document).ready(function () {
+    $('.product__content').mixItUp({
+      selectors: {
+        filter: '.filter-1'
+      }
+    });
+
+    $('.desing__content').mixItUp({
+      selectors: {
+        filter: '.filter-2'
+      }
+    });
+  });
+
 });
